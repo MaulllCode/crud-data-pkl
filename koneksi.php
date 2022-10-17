99b614570e3218c4dc1bf781a2f0883109f0bff2
maulana <maulaadjis52@gmail.com>
2022-10-17 13:01:24 +0700

    update

--- koneksi.php
+++ koneksi.php
@@ -2,5 +2,5 @@
 $kon = mysqli_connect("localhost", "root", "", "crud-pkl");
 
 if (!$kon) {
-    echo "<script>alert('Koneksi error!');window.location.href='home    .php';</script>";
+    echo "<script>alert('Koneksi error!');window.location.href='home.php';</script>";
 }
